2a06508bf36725cfa088c38287d789148da6342a
theElusiveJoe <flvlimpo@gmail.com>
2023-12-16 22:18:28 +0300

    mc не нужны кастомные имена

--- src/monoid/controller.py
+++ src/monoid/controller.py
@@ -7,14 +7,14 @@ class MonoidController:
     generators: list[Universe]
     names: list[str]
 
-    def __init__(self, seq: list[Universe], names: list[str] | None = None) -> None:
+    def __init__(self, seq: list[Universe]) -> None:
         self.universe_type = type(seq[0])
 
         if seq[0].identity() in seq:
             raise RuntimeError(f'identity elem can not be generator')
 
         self.generators = seq
-        self.names = names if names else [chr(i) for i in range(
+        self.names = [chr(i) for i in range(
             ord('a'), ord('a')+len(self.generators))]
 
     def identity(self) -> Universe:
