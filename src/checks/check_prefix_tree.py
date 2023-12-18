1bb41b64fe18397951ae36601e24527ba17dae93
theElusiveJoe <flvlimpo@gmail.com>
2023-12-18 04:12:58 +0300

    add some stupid prefix tree work validation skript

--- src/checks/check_prefix_tree.py
+++ src/checks/check_prefix_tree.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
-import itertools
+import sys
+sys.path.append(f'{sys.path[0]}/..')
+print(sys.path)
 
 from monoid import MonoidElem
 from algos.factorized.bs_prefix_tree import PrefixTree, PrefixTreeNode
@@ -7,7 +9,8 @@ from universes import Transformation
 
 
 def fill_tree(tree: PrefixTree, string: MonoidElem, sigma: list[int], l:int):
-    tree.insert(string, Transformation([]))
+    tree.insert(string, Transformation([]), skip_if_suff_not_exists=False)
+    assert tree.find_node(string) is not None, print(f'insert failed {string}')
 
     if l == 0:
         return
@@ -18,8 +21,9 @@ def fill_tree(tree: PrefixTree, string: MonoidElem, sigma: list[int], l:int):
 sigma = [1,2,3,4]
 tree = PrefixTree([], Transformation([]))
 fill_tree(tree, MonoidElem.identity(), sigma, 5)
+print(tree)
 # print(tree)
-w = MonoidElem([2,2])
+w = MonoidElem([1,2,3])
 
 print(f'w is {w}')
 res = tree.search_superstrings(w)
