9bac5712d1dcf8a2bb5752263ce370f40957033e
theElusiveJoe <flvlimpo@gmail.com>
2023-12-21 06:09:14 +0300

    change utils

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,5 +1,39 @@
+import logging
+from logging import StreamHandler
+import sys
+
+
+class _MyLogger:
+    l: logging.Logger
+    h: StreamHandler
+
+    def __init__(self):
+        self.l = my_logger = logging.Logger('my_logger')
+        self.l.setLevel(logging.INFO)
+        self.h = StreamHandler(stream=sys.stdout)
+
+    def log(self, s: str):
+        self.l.info(s)
+
+    def enable(self):
+        self.l.addHandler(self.h)
+
+    def disable(self):
+        self.l.removeHandler(self.h)
+
+
+_logger_instance = _MyLogger()
+
+
 def log(s: str, lvl=1):
     assert lvl > 0
-    print(
-        f"{' '*4*(lvl-1)}-> {s}"
-    )
+    s = f"{' '*4*(lvl-1)}-> {s}"
+    _logger_instance.log(s)
+
+
+def enable_logging():
+    _logger_instance.enable()
+
+
+def disable_logging():
+    _logger_instance.disable()
