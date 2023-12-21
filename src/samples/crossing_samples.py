adf211cb54911cfa6a9f1bb834996bac3e93e399
theElusiveJoe <flvlimpo@gmail.com>
2023-12-21 19:56:54 +0300

    absolute_randomic_tests

--- src/samples/crossing_samples.py
+++ src/samples/crossing_samples.py
@@ -1,5 +1,7 @@
 from universes.transformations import Transformation
 from generating_sets import GeneratingSetsFamily
+import random
+
 
 build_gsf = GeneratingSetsFamily.build_from_description
 
@@ -77,3 +79,67 @@ two_independend = build_gsf(
     Transformation,
     sg1,sg2,transit
 )
+
+'''
+две независимые полугруппы и переходики между ними
+'''
+
+def random_list(a: int, b: int, l: int):
+    '''
+    возвращает список длинны l с рандомными числами от a до b включительно
+    '''
+    return [random.randint(a,b) for _ in range(l)]
+
+
+def gen_2_semigs(l1: int, l2:int, s1: int, s2: int, s3: int):
+    '''
+    l1 - мощность множества, на которое действует первая полугруппа
+
+    l2 - мощность множества, на которое действует вторая полугруппа
+
+    s1 - количество порождающих первую полугруппу
+    
+    s2 - количество порождающих вторую полугруппу
+
+    s3 - количество свапов между элементами групп
+    '''
+    const1 = [i+1 for i in range(l1)]
+    const2 = [l1+i+1 for i in range(l2)]
+
+    e1 = lambda: random_list(1, l1, l1)
+    e2 = lambda: random_list(l1+1, l2+l1, l2)
+
+    sg1 = [
+        e1() + const2
+        for _ in range(s1) 
+    ]
+    sg2 = [
+        const1 + e2()
+        for _ in range(s2) 
+    ]
+
+    e3 = lambda: random.choice([e1,e2])() + random.choice([e1,e2])() 
+    trans = [
+        e3()
+        for _ in range(s3)
+    ]
+    print('sg1:', sg1)
+    print('sg2:', sg2)
+    print('sg3:', trans)
+    return build_gsf(
+    Transformation,
+    sg1,sg2,trans
+    )
+
+
+'''
+две произвольные полугруппы
+'''
+
+
+def gen_2_random(t: int, s1: int, s2:int):
+    return build_gsf(
+        Transformation,
+        [random_list(1, t, t) for _ in range(s1)],
+        [random_list(1, t, t) for _ in range(s2)],
+    )
